b3c0a3f3e8f68eb09c8d308380df8590c14d6491
Manju Kumari <manju.kumari@encodingenhancers.com>
2023-10-23 00:36:43 +0530

    Add project files.

---
+++ TaskOrganiserWeb/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskOrganiserWeb.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string TaskName { get; set; } 
+        public string Description { get; set; }
+        public required string Status { get; set; }
+        public DateTime CreatedDateTime { get; set; }=DateTime.Now;
+        //public Task()
+        //{
+        //    this.Id = 0;
+        //    this.TaskId = 0;
+        //    this.TaskName = "";
+        //    this.Description = "";
+        //    this.Status = "";
+        //    this.CreatedDateTime = CreatedDateTime;
+        //}
+    }
+}
